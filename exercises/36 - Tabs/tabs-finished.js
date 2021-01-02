9f7dfe72adc3a09a405526c306915324e328cd52
aslan <howdy@aslanfrench.work>
2021-01-01 19:58:51 -0600

    finished lesson 36

--- exercises/36 - Tabs/tabs-finished.js
+++ exercises/36 - Tabs/tabs-finished.js
@@ -1,19 +1,21 @@
-const tabs = document.querySelector('.tabs');
-const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+const tabs = document.querySelector(`.tabs`);
+const tabButtons = tabs.querySelectorAll(`[role="tab"]`);
+const tabPanels = Array.from(tabs.querySelectorAll(`[role="tabpanel"]`));
 
 function handleTabClick(event) {
   // hide all tab panels
   tabPanels.forEach(panel => {
     panel.hidden = true;
   });
+
   // mark all tabs as unselected
   tabButtons.forEach(tab => {
     // tab.ariaSelected = false;
-    tab.setAttribute('aria-selected', false);
+    tab.setAttribute(`aria-selected`, false);
   });
+
   // mark the clicked tab as selected
-  event.currentTarget.setAttribute('aria-selected', true);
+  event.currentTarget.setAttribute(`aria-selected`, true);
   // find the associated tabPanel and show it!
   const { id } = event.currentTarget;
 
@@ -27,9 +29,9 @@ function handleTabClick(event) {
   // METHOD 2 - find in the array of tabPanels
   console.log(tabPanels);
   const tabPanel = tabPanels.find(
-    panel => panel.getAttribute('aria-labelledby') === id
+    panel => panel.getAttribute(`aria-labelledby`) === id
   );
   tabPanel.hidden = false;
 }
 
-tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
+tabButtons.forEach(button => button.addEventListener(`click`, handleTabClick));
