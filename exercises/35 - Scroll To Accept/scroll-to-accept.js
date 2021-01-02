6163866ed37600b7f78f1717cd08531772ccdadb
aslan <howdy@aslanfrench.work>
2021-01-01 18:51:41 -0600

    finished lesson 35

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,18 @@
-console.log('IT WORKS!');
+const terms = document.querySelector(`.terms-and-conditions`);
+const watch = document.querySelector(`.watch`);
+const button = document.querySelector(`.accept`);
+
+const observer = new IntersectionObserver(observerCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+function observerCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    //
+    observer.unobserve(terms.lastElementChild);
+  }
+}
+
+observer.observe(terms.lastElementChild);
